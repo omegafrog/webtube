6cd45f1976077fae504990d5f01a7c469e62d26c
omegafrog <jiwoo22110103@gmail.com>
2021-10-23 13:08:33 +0900

    feat: get video info
    using mixin and iteration get video info
    
    issue:returning html #6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,8 +3,56 @@ const fakeUser = {
   loggedIn: true,
 };
 
+const videos = [
+  {
+    name: "first video",
+    rating: 3,
+    createdAt: "2 minutes ago",
+    comments: [
+      {
+        name: "joker",
+        comment: "why so serious",
+      },
+      {
+        name: "faker",
+        comment: "championship winner is me!",
+      },
+    ],
+  },
+  {
+    name: "second video",
+    rating: 5,
+    createdAt: "2.5 minutes ago",
+    comments: [
+      {
+        name: "joker",
+        comment: "why so serious",
+      },
+      {
+        name: "faker",
+        comment: "championship winner is me!",
+      },
+    ],
+  },
+  {
+    name: "third video",
+    rating: 1,
+    createdAt: "3 minutes ago",
+    comments: [
+      {
+        name: "joker",
+        comment: "why so serious",
+      },
+      {
+        name: "faker",
+        comment: "championship winner is me!",
+      },
+    ],
+  },
+];
+
 export const recommended = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser, videos });
 export const searchVideo = (req, res) => res.send("search video");
 export const seeVideo = (req, res) => res.render("watch");
 export const editVideo = (req, res) => res.send("edit video");
