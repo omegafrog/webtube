9a0d3faa46252cd3ebece733e9fa44fecfa31722
omegafrog <jiwoo22110103@gmail.com>
2021-10-27 10:48:48 +0900

    dev: kill server before restart
    because of an error port:4000, kill server before nodemon restart server

--- src/init.js
+++ src/init.js
@@ -6,4 +6,13 @@ const port = 4000;
 
 const handleListener = () => console.log("success");
 
+app.once("SIGUSR2", function () {
+  app.kill(app.pid, "SIGUSR2");
+});
+
+app.on("SIGINT", function () {
+  // this is only called on ctrl+c, not restart
+  app.kill(app.pid, "SIGINT");
+});
+
 app.listen(port, handleListener);
