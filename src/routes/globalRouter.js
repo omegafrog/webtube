81e07d825b0f96d41dcb4f7713225190c7c2cafe
omegafrog <jiwoo22110103@gmail.com>
2021-10-29 12:41:33 +0900

    dev: load userController
    issue:make user login feature #21

--- src/routes/globalRouter.js
+++ src/routes/globalRouter.js
@@ -1,11 +1,13 @@
+// TODO: #23 change globalRouter to rootRouter @omegafrog
+
 import express from "express";
-import { loginUser, joinUser } from "../controllers/userController";
+import { loginUser, getJoin, postJoin } from "../controllers/userController";
 import { recommended, searchVideo } from "../controllers/videoController";
 const globalRouter = express.Router();
 
 globalRouter.get("/", recommended);
 globalRouter.get("/login", loginUser);
 globalRouter.get("/search", searchVideo);
-globalRouter.get("/join", joinUser);
+globalRouter.route("/join").get(getJoin).post(postJoin);
 
 export default globalRouter;
