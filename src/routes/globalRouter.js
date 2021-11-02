eca6ce049fdb0ba77318d2b0f3686a7cc6e3902c
omegafrog <jiwoo22110103@gmail.com>
2021-11-02 15:17:23 +0900

    feat : make middleware to restrict access
    if user is not logged in, go to login page when he access protected URL
    
    if user is logged in, go to homepage when he access login page.

--- src/routes/globalRouter.js
+++ src/routes/globalRouter.js
@@ -9,11 +9,20 @@ import {
   logout,
 } from "../controllers/userController";
 import { recommended, searchVideo } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 const globalRouter = express.Router();
 
 globalRouter.get("/", recommended);
-globalRouter.route("/login").get(getLogin).post(postLogin);
-globalRouter.get("/search", searchVideo);
-globalRouter.route("/join").get(getJoin).post(postJoin);
+globalRouter
+  .route("/login")
+  .all(protectorMiddleware)
+  .get(getLogin)
+  .post(postLogin);
+globalRouter.get("/search", protectorMiddleware, searchVideo);
+globalRouter
+  .route("/join")
+  .all(protectorMiddleware)
+  .get(getJoin)
+  .post(postJoin);
 
 export default globalRouter;
