077c154fa548a1fcb85ea56c76416db82fc3b518
omegafrog <jiwoo22110103@gmail.com>
2021-11-03 14:48:04 +0900

    bug : cannot load user in pugfile
    because locals.user get user data from session,
    not logged in user cannot load user data.
    so if user not logged in, locals.user will be {}.
    
    issue:social login #30

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,7 +3,9 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   if (req.session.user) {
-    res.locals.user = req.session.user || {};
+    res.locals.user = req.session.user;
+  } else {
+    res.locals.user = {};
   }
   next();
 };
